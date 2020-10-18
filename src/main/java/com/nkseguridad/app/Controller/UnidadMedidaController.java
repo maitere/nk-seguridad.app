94ba584c797795678ba2d1b29684e72a006d712f
maitere <62911232+maitere@users.noreply.github.com>
2020-10-18 10:17:54 -0500

    Update UnidadMedidaController.java

--- src/main/java/com/nkseguridad/app/Controller/UnidadMedidaController.java
+++ src/main/java/com/nkseguridad/app/Controller/UnidadMedidaController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nkseguridad.app.Entity.Ruta;
@@ -17,6 +19,7 @@ import com.nkseguridad.app.Service.IUnidadMedidaService;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.POST})
 public class UnidadMedidaController {
 
 	@Autowired
